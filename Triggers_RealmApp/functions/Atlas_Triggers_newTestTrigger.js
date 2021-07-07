913ec81c61316195f9e0cf2f60f3eb498f712c2b
David Stewart <davidstewart@Davids-MacBook-Pro.local>
2021-07-07 08:20:33 +0100

    Added test trigger

---
+++ Triggers_RealmApp/functions/Atlas_Triggers_newTestTrigger.js
@@ -0,0 +1,14 @@
+/**
+ * When the workersEntity collection contains a document that has a reference to the 
+ * updated document's bullhorn ID, add the updated document to the 360 worker collection
+ * along with the universal ID.
+ * 
+ * @param {Object} changeEvent
+ *   Contains details about the updated document. The fullDocument property of this Object
+ *   contains all document's fields with their existing and updated values.
+ */
+exports = async function(changeEvent, func2) {
+  console.log('Updating Bullhorn TEST >>>>>>>>>>>>>>>>>>> Candiate...');
+  
+  return true;
+}
